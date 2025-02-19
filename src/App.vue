dd7cb0b38f33ad80eeaba1d973f24664cf8f0149
emmet-opinionx <emmet@opinionx.co>
2025-02-19 12:37:27 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-main>
+      <CustomTextField>
+        <template #prepend>
+          <v-icon>mdi-email</v-icon>
+        </template>
+      </CustomTextField>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import CustomTextField from "./components/CustomTextField.vue";
+</script>
---
+++ src/components/CustomTextField.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-text-field>
+    <template #prepend>
+      <div class="text-red">
+        <slot name="prepend"></slot>
+      </div>
+    </template>
+  </v-text-field>
+</template>
+
+<script setup lang="ts"></script>
